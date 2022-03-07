b0af6ffd0ff90bc384121a15a36633a908e6d677
Joao Victor Lima Viana <joaovlimaviana@gmail.com>
2022-03-07 05:21:28 -0300

    Arquivos da aula 4

---
+++ template-strings.js
@@ -0,0 +1,8 @@
+const nome = "João";
+const idade = 2022 - 1996;
+const cidadeNascimento = "Maceió";
+
+// const apresentacao = "Meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeNascimento;
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeNascimento}`;
+console.log(apresentacao);
