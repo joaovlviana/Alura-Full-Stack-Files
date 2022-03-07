c18484a3daa0544dfcfa48a1d874f10a05738944
Joao Victor Lima Viana <joaovlimaviana@gmail.com>
2022-03-06 22:26:15 -0300

    Exercícios aula 2

---
+++ conversoes.js
@@ -0,0 +1,15 @@
+// tipo de dado
+// booleanos
+
+//conversao implícita
+const numero = 456;
+const numeroString = '456';
+
+console.log(numero == numeroString); //true
+console.log(numero + numeroString); //456456
+
+//conversao explicita
+//Number()
+//String()
+
+console.log(numero + Number(numeroString)); //912
