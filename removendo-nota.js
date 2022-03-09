426aee91a5c22e518afe67e7d8a03f8646ebd752
Joao Victor Lima Viana <joaovlimaviana@gmail.com>
2022-03-09 15:52:00 -0300

    Arquivos aulas sobre arrays em javascript

--- removendo-nota.js
+++ removendo-nota.js
@@ -1,7 +1,6 @@
-const notas = [10, 7, 8, 5, 10];
-notas.pop(); //remove o último elemento da lista
+const notas = [10 ,7 ,8, 5, 10]
+notas.pop()
+console.log(notas)
 
-let media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
-
-console.log(notas);
-console.log(`A média é ${media}`);
+let media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length
+console.log(`A média é ${media}`)
